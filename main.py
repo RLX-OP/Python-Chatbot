4b5ed91ee56651c82e4395863ed6928ccce7cb12
RLX <85926055+RLX-OP@users.noreply.github.com>
2022-11-19 22:44:00 +0530

    Update main.py
    Looped the code so it doesn't turn off

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-import requests, os, time, sys
+import requests, time
 
-# Typing Effect in Console
-def typing(words):
-	for c in words:
-		sys.stdout.write(c)
-		sys.stdout.flush()
-		time.sleep(0.10)
 
-# Settings
-name = "Enter your name here"
-msg = input(name + ":-  ")
+def chatBot():
+        name = "Enter Your name here"
+        msg = input(name + ":-  ")
 
 # API url for chat response
-url = "http://api.brainshop.ai/get?bid=169766&key=m6HzHyHVG3qvz7LO&uid="+name+"&msg="+msg+""
+        url = "http://api.brainshop.ai/get?bid=169766&key=m6HzHyHVG3qvz7LO&uid="+name+"&msg="+msg+""
 
 # Connecting to API and converting the data to JSON format.
-req = requests.get(url = url)
-data = req.json()
+        req = requests.get(url = url)
+        data = req.json()
 
 # Name Of JSON key that will give out a value
-response = data['cnt']
+        response = data['cnt']
 
 # Data responded by API if status is 200, if not then throws an error in console
-if req.status_code == 200:
-        time.sleep(1)
-        typing("ChatBot:-  " + response)
-        print("\n")
-        os.execl(sys.executable, 'python', __file__, *sys.argv[1:])
-        
-                
-else:
-       typing("An Error Occurred In API Please Restart And Try Again")
+        if req.status_code == 200:
+                time.sleep(2)
+                print("ChatBot:-  " + response)
+                print("\n")
+        else:
+                print("An Error Occurred In API Please Restart And Try Again")
+                exit()
+
+while True:
+        chatBot()
